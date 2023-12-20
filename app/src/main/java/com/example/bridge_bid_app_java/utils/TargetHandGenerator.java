4b3793c76ead3978b9c8e514cc633eba182d9338
Jared Williams <jared.williams101697@gmail.com>
2023-12-19 20:27:00 -0500

    Finished adding responses to a one heart bid

--- app/src/main/java/com/example/bridge_bid_app_java/utils/TargetHandGenerator.java
+++ app/src/main/java/com/example/bridge_bid_app_java/utils/TargetHandGenerator.java
@@ -48,6 +48,26 @@ public class TargetHandGenerator {
         return hand;
     }
 
+    private static List<Card> fillRandomHand(List<Card> preselectedCards, List<Card> leftOut) {
+        List<Card> hand = new ArrayList<>(preselectedCards);
+        List<Card> cards = new ArrayList<>(Arrays.asList(Card.values()));
+        cards.removeAll(preselectedCards);
+        cards.removeAll(leftOut);
+        Random random = new Random();
+        int bound = cards.size();
+
+        while (hand.size() != 13) {
+            int n = random.nextInt(bound);
+            Card card = cards.get(n);
+            cards.remove(card);
+            card.isSelected(true);
+            hand.add(card);
+            bound--;
+        }
+
+        return hand;
+    }
+
     private static List<Card> fillRandomHandWithout(List<Card> preselectedCards) {
         List<Card> hand = new ArrayList<>();
         List<Card> cards = new ArrayList<>(Arrays.asList(Card.values()));
@@ -100,6 +120,19 @@ public class TargetHandGenerator {
 
         do {
             hand = new Hand(fillRandomHand(cards));
+        } while (hand.getTotalPointCount() != totalPoints ||
+                hand.getSpades() != spades ||
+                hand.getHearts() != hearts ||
+                hand.getDiamonds() != diamonds ||
+                hand.getClubs() != clubs);
+
+        return hand;
+    }
+    public static Hand createTargetedHand(int totalPoints, int spades, int hearts, int diamonds, int clubs, List<Card> cards, List<Card> notHeld) {
+        Hand hand;
+
+        do {
+            hand = new Hand(fillRandomHand(cards, notHeld));
         } while (hand.getTotalPointCount() != totalPoints ||
                 hand.getSpades() != spades ||
                 hand.getHearts() != hearts ||
